c4b9681c9f493c67666d641689401128803937c6
Luciana Boasso <luboasso16@gmail.com>
2023-12-01 00:45:42 +0100

    Add icon to products

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useEffect } from "react";
 import { ProductsContext } from "../../context/ProductsContext/ProductsState";
 import { Card } from "antd";
 import "./Products.scss";
+import { ShoppingCartOutlined } from "@ant-design/icons";
 
 const Products = () => {
   const { products, getProducts } = useContext(ProductsContext);
@@ -19,9 +20,11 @@ const Products = () => {
             title={product.product_name}
             bordered={false}
             style={{ width: 300 }}
+            
           >
             <p>{product.description}</p>
             <p>{product.price}</p>
+            <ShoppingCartOutlined />
           </Card>
         );
       })}
